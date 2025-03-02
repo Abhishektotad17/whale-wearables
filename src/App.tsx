062de074fdc75c640f4630e703599e011c99ab08
Abhishek Totad <abhishek.t@heraizen.com>
2025-03-02 20:42:35 +0530

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Camera } from "lucide-react";
+import React from "react";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return(
+    <>
+    <Navbar />
+    </>
+    
+  );
+  // return <Camera color="red" size={48} />;
+}
+
+export default App
